284c51742bab0378128230cd009bc876f4e72503
yasyasinbc <yasyasinbc@gmail.com>
2023-05-18 00:39:30 +0200

    version 1, added time measure

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/yasinbc/AlgoritmiaComplejidad/ordenacion"
+	"time"
 )
 
 func main() {
 	array := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+
+	start := time.Now()
 	fmt.Println(ordenacion.Burbuja(array))
+	elapsed := time.Since(start)
+	fmt.Println("==elapsed time==", elapsed)
+
 }
