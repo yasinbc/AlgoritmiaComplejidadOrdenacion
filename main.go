012199a93594930b2b6c15f7becea23cb471332b
yasyasinbc <yasyasinbc@gmail.com>
2023-05-18 00:50:55 +0200

    version 2, runtime measure

--- main.go
+++ main.go
@@ -9,9 +9,10 @@ import (
 func main() {
 	array := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
 
-	start := time.Now()
+	startTime := time.Now()
 	fmt.Println(ordenacion.Burbuja(array))
-	elapsed := time.Since(start)
-	fmt.Println("==elapsed time==", elapsed)
+	endTime := time.Now()
+	fmt.Println("Tiempo al empezar: ", startTime)
+	fmt.Println("Tiempo al terminar: ", endTime)
 
 }
