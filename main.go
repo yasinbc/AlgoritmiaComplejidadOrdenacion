0cd1c330211f912684d05ea23383c5e3209d1c22
yasyasinbc <yasyasinbc@gmail.com>
2023-05-20 17:44:22 +0200

    Mejora en tiempo

--- main.go
+++ main.go
@@ -20,34 +20,35 @@ func main() {
 	fmt.Println("\n\n\nAlgoritmo de Ordenacion Burbuja: ")
 	startTime1 := time.Now()
 	ordenacion.Burbuja(array)
-	fmt.Println("Tiempo de ejecucion: ", time.Since(startTime1))
+	fmt.Println("Tiempo de ejecucion: ", time.Since(startTime1).Nanoseconds(), "ns")
 
 	fmt.Println("\n")
 
 	fmt.Println("Algoritmo de Ordenacion por Seleccion: ")
 	startTime2 := time.Now()
 	ordenacion.Seleccion(array)
-	fmt.Println("Tiempo de ejecucion: ", time.Since(startTime2))
+	fmt.Println("Tiempo de ejecucion: ", time.Since(startTime2).Nanoseconds(), "ns")
 	fmt.Println("\n")
 
 	fmt.Println("Algoritmo de Ordenacion por Insercion: ")
 	startTime3 := time.Now()
 	ordenacion.Insercion(array)
-	fmt.Println("Tiempo de ejecucion: ", time.Since(startTime3))
+	fmt.Println("Tiempo de ejecucion: ", time.Since(startTime3).Nanoseconds(), "ns")
 	fmt.Println("\n")
 
 	fmt.Println("Algoritmo de Ordenacion Merge Sort: ")
 	startTime4 := time.Now()
 	ordenacion.MergeSort(array)
-	fmt.Println("Tiempo de ejecucion: ", time.Since(startTime4))
+	fmt.Println("Tiempo de ejecucion: ", time.Since(startTime4).Nanoseconds(), "ns")
 	fmt.Println("\n")
 
 	fmt.Println("Algoritmo de Ordenacion Quick Sort: ")
 	startTime5 := time.Now()
 	ordenacion.QuickSort(array)
-	fmt.Println("Tiempo de ejecucion: ", time.Since(startTime5))
+	fmt.Println("Tiempo de ejecucion: ", time.Since(startTime5).Nanoseconds(), "ns")
 	fmt.Println("\n")
 
-	fmt.Println("\n\n\n\nTiempo de ejecucion de todo el programa: ", time.Since(tiempoPrograma))
+	fmt.Println("\n\n\n\nArray de ", size, " elementos a ordenar.")
+	fmt.Println("\nTiempo de ejecucion de todo el programa: ", time.Since(tiempoPrograma).Nanoseconds(), "ns\n")
 
 }
