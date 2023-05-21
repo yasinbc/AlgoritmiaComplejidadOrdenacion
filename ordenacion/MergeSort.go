e10c4c2947c6928adf1654d665cadeea7b4e7485
yasyasinbc <yasyasinbc@gmail.com>
2023-05-21 16:00:53 +0200

    Modificada lectura de algoritmo Merge Sort

--- ordenacion/MergeSort.go
+++ ordenacion/MergeSort.go
@@ -1,112 +1,47 @@
 package ordenacion
 
-func sort(A []int) []int {
-	if len(A) <= 1 {
-		return A
+func Sort(array []int) []int {
+	if len(array) <= 1 {
+		return array
 	}
 
-	left, right := split(A)
-	left = sort(left)
-	right = sort(right)
-	return merge(left, right)
+	izquierda, derecha := split(array)
+	izquierda = Sort(izquierda)
+	derecha = Sort(derecha)
+	return merge(izquierda, derecha)
 }
 
-// split array into two
-func split(A []int) ([]int, []int) {
-	return A[0 : len(A)/2], A[len(A)/2:]
+// Divide el array en 2
+func split(array []int) ([]int, []int) {
+	return array[0 : len(array)/2], array[len(array)/2:]
 }
 
-// assumes that A and B are sorted
-func merge(A, B []int) []int {
-	arr := make([]int, len(A)+len(B))
+// Asume que arrayA y arrayB estan ordenadas
+func merge(arrayA, arrayB []int) []int {
+	arr := make([]int, len(arrayA)+len(arrayB))
 
-	// index j for A, k for B
+	// index j para arrayA, k para arrayB
 	j, k := 0, 0
 
 	for i := 0; i < len(arr); i++ {
-		// fix for index out of range without using sentinel
-		if j >= len(A) {
-			arr[i] = B[k]
+		if j >= len(arrayA) {
+			arr[i] = arrayB[k]
 			k++
 			continue
-		} else if k >= len(B) {
-			arr[i] = A[j]
+		} else if k >= len(arrayB) {
+			arr[i] = arrayA[j]
 			j++
 			continue
 		}
-		// default loop condition
-		if A[j] > B[k] {
-			arr[i] = B[k]
+
+		if arrayA[j] > arrayB[k] {
+			arr[i] = arrayB[k]
 			k++
 		} else {
-			arr[i] = A[j]
+			arr[i] = arrayA[j]
 			j++
 		}
 	}
 
 	return arr
 }
-
-//func MergeSort(items []int) []int {
-//	if len(items) < 2 {
-//		return items
-//	}
-//	first := MergeSort(items[:len(items)/2])
-//	second := MergeSort(items[len(items)/2:])
-//	return Merge(first, second)
-//}
-//
-//func Merge(a []int, b []int) []int {
-//	final := []int{}
-//	i := 0
-//	j := 0
-//	for i < len(a) && j < len(b) {
-//		if a[i] < b[j] {
-//			final = append(final, a[i])
-//			i++
-//		} else {
-//			final = append(final, b[j])
-//			j++
-//		}
-//	}
-//	for ; i < len(a); i++ {
-//		final = append(final, a[i])
-//	}
-//	for ; j < len(b); j++ {
-//		final = append(final, b[j])
-//	}
-//	return final
-//}
-
-//func MergeSort(array []int) []int {
-//	if len(array) < 2 {
-//		return array
-//	}
-//
-//	mitad := (len(array)) / 2
-//	return Merge(MergeSort(array[:mitad]), MergeSort(array[mitad:]))
-//
-//}
-//
-//func Merge(izquierda, derecha []int) []int {
-//
-//	size, i, j := len(izquierda)+len(derecha), 0, 0
-//	array := make([]int, size, size)
-//
-//	for k := 0; k < size; k++ {
-//		if i > len(izquierda)-1 && j <= len(derecha)-1 {
-//			array[k] = derecha[j]
-//			j++
-//		} else if j > len(derecha)-1 && i <= len(izquierda)-1 {
-//			array[k] = izquierda[i]
-//			i++
-//		} else if izquierda[i] < derecha[j] {
-//			array[k] = izquierda[i]
-//			i++
-//		} else {
-//			array[k] = derecha[j]
-//			j++
-//		}
-//	}
-//	return array
-//}
