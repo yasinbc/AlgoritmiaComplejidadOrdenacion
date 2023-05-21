4908a7b33c2b894b5c8f9a4b6f9987f59ffd31cb
yasyasinbc <yasyasinbc@gmail.com>
2023-05-21 14:11:35 +0200

    Vista sólo del rendimiento de los algoritmos

--- main.go
+++ main.go
@@ -8,13 +8,13 @@ import (
 
 func main() {
 	//Incluye visualizacion de las matrices
-	var size int = 10
+	var size int = 100
 	array := ordenacion.RanNumArr(size)
 
 	fmt.Println("\n\n\n\nArray de ", size, " elementos a ordenar.\n")
 	tiempoPrograma := time.Now()
 
-	fmt.Println("\n", array, "\n")
+	fmt.Println(array, "\n")
 	fmt.Println("	- Algoritmo de Ordenacion Burbuja: ")
 	startTime1 := time.Now()
 	ordenacion.Burbuja(array)
@@ -23,12 +23,10 @@ func main() {
 	fmt.Println("			Milisegundos:  ", time.Since(startTime1).Milliseconds())
 	fmt.Println("			Microsegundos: ", time.Since(startTime1).Microseconds())
 	fmt.Println("			Nanosegundos:  ", time.Since(startTime1).Nanoseconds(), "\n")
-	fmt.Println("\n", array, "\n")
 
 	array = nil
 	array = ordenacion.RanNumArr(size)
 
-	fmt.Println("\n", array, "\n")
 	fmt.Println("	- Algoritmo de Ordenacion por Seleccion: ")
 	startTime2 := time.Now()
 	ordenacion.Seleccion(array)
@@ -37,12 +35,10 @@ func main() {
 	fmt.Println("			Milisegundos:  ", time.Since(startTime2).Milliseconds())
 	fmt.Println("			Microsegundos: ", time.Since(startTime2).Microseconds())
 	fmt.Println("			Nanosegundos:  ", time.Since(startTime2).Nanoseconds(), "\n")
-	fmt.Println("\n", array, "\n")
 
 	array = nil
 	array = ordenacion.RanNumArr(size)
 
-	fmt.Println("\n", array, "\n")
 	fmt.Println("	- Algoritmo de Ordenacion por Insercion: ")
 	startTime3 := time.Now()
 	ordenacion.Insercion(array)
@@ -51,12 +47,10 @@ func main() {
 	fmt.Println("			Milisegundos:  ", time.Since(startTime3).Milliseconds())
 	fmt.Println("			Microsegundos: ", time.Since(startTime3).Microseconds())
 	fmt.Println("			Nanosegundos:  ", time.Since(startTime3).Nanoseconds(), "\n")
-	fmt.Println("\n", array, "\n")
 
 	array = nil
 	array = ordenacion.RanNumArr(size)
 
-	fmt.Println("\n", array, "\n")
 	fmt.Println("	- Algoritmo de Ordenacion Merge Sort: ")
 	startTime4 := time.Now()
 	ordenacion.MergeSort(array, 0, len(array)-1)
@@ -65,12 +59,10 @@ func main() {
 	fmt.Println("			Milisegundos:  ", time.Since(startTime4).Milliseconds())
 	fmt.Println("			Microsegundos: ", time.Since(startTime4).Microseconds())
 	fmt.Println("			Nanosegundos:  ", time.Since(startTime4).Nanoseconds(), "\n")
-	fmt.Println("\n", array, "\n")
 
 	array = nil
 	array = ordenacion.RanNumArr(size)
 
-	fmt.Println("\n", array, "\n")
 	fmt.Println("	- Algoritmo de Ordenacion Quick Sort: ")
 	startTime5 := time.Now()
 	ordenacion.QuickSort(array)
@@ -79,9 +71,7 @@ func main() {
 	fmt.Println("			Milisegundos:  ", time.Since(startTime5).Milliseconds())
 	fmt.Println("			Microsegundos: ", time.Since(startTime5).Microseconds())
 	fmt.Println("			Nanosegundos:  ", time.Since(startTime5).Nanoseconds(), "\n")
-	fmt.Println("\n", array, "\n")
 
-	fmt.Println("\n", array, "\n")
 	fmt.Println("Tiempo de ejecucion de todo el programa: ")
 	fmt.Println("	Segundos:      ", time.Since(tiempoPrograma).Seconds())
 	fmt.Println("	Milisegundos:  ", time.Since(tiempoPrograma).Milliseconds())
